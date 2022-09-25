5b316e54f18262fcdac94584d14f5df0a9a71e48
yijing-wu <yijingwu.w@gmail.com>
2022-09-25 14:53:54 -0400

    Refactor featured Project

--- src/components/Work/FeaturedProject.tsx
+++ src/components/Work/FeaturedProject.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-import Grid from "@mui/material/Grid";
-import OpenInNewIcon from "@mui/icons-material/OpenInNew";
-
 import CameraCalibrationProfile from "../../assets/projects/camera-calibration.png";
 import FeaturedProjectWidget from "./FeaturedProjectWidget";
 
@@ -30,9 +27,7 @@ export default function FeaturedProject() {
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio eu feugiat. Felis bibendum ut tristique et.",
       tools: ["VS", "Code", "NPM", "XCode", "Java"],
-      // mainLink: "/",
       githubLink: "/",
-      // buttonLink: "/",
     },
     {
       picture: CameraCalibrationProfile,
@@ -40,24 +35,11 @@ export default function FeaturedProject() {
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio eu feugiat. Felis bibendum ut tristique et.",
       tools: ["VS", "Code", "NPM", "XCode", "Java"],
-      // mainLink: "/",
-      // externalLink: null,
-      // githubLink: "/",
-      // buttonLink: "/",
     },
   ];
 
   return (
     <div style={{}}>
-      {/* <FeaturedProjectWidget
-        index={1}
-        picture={CameraCalibrationProfile}
-        title={"Lorem Ipsum Dolor"}
-        description={
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio eu feugiat. Felis bibendum ut tristique et."
-        }
-        tool={"VS Code HEUI"}
-      /> */}
       {featuredProjects.map((featuredProject, index) => {
         return (
           <FeaturedProjectWidget
@@ -73,34 +55,6 @@ export default function FeaturedProject() {
           />
         );
       })}
-
-      {/* <Grid container>
-        <Grid item xs={7} style={{ backgroundColor: "red" }}>
-          <img src={CameraCalibrationProfile} style={{ width: "100%" }}></img>
-        </Grid>
-        <Grid
-          item
-          xs={5}
-          style={{ textAlign: "right", marginTop: 10, position: "relative" }}
-        >
-          <div>Featured Project</div>
-          <div>Lorem Ipsum Dolor</div>
-          <div
-            style={{
-              backgroundColor: "blue",
-              width: "120%",
-              height: 150,
-              left: "-20%",
-              position: "absolute",
-            }}
-          >
-            {
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio eu feugiat. Felis bibendum ut tristique et. "
-            }
-          </div>
-          <div style={{ position: "relative", top: 150 }}>VS Code HEUI</div>
-        </Grid>
-      </Grid> */}
     </div>
   );
 }
--- src/components/Work/FeaturedProjectWidget.tsx
+++ src/components/Work/FeaturedProjectWidget.tsx
@@ -12,6 +12,175 @@ import {
   myTextGrey,
 } from "../../MyLibrary/MyColors";
 
+interface FeaturedProjectImageProps {
+  picture?: any;
+  title: string;
+}
+
+function FeaturedProjectImage({ picture, title }: FeaturedProjectImageProps) {
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          backgroundColor: "#35425980", // myNavy with 80 Aplha
+          position: "absolute",
+          zIndex: 1,
+          height: "100%",
+        }}
+      />
+      <img
+        src={picture}
+        style={{ width: "100%" }}
+        alt={title + " profile Picture"}
+      />
+    </>
+  );
+}
+
+interface FeaturedProjectDetailsProps {
+  textRightAlign: boolean;
+  title: string;
+  description: React.ReactNode;
+  tools: string[];
+  externalLink?: string;
+  githubLink?: string;
+  buttonLink?: string;
+}
+
+function FeaturedProjectDetails({
+  textRightAlign,
+  title,
+  description,
+  tools,
+  externalLink,
+  githubLink,
+  buttonLink,
+}: FeaturedProjectDetailsProps) {
+  const [linkMouseFocused, setLinkMouseFocused] = useState("none");
+
+  return (
+    <>
+      <Text
+        style={{
+          color: myMintGreen,
+          fontFamily: "SFMono-Regular",
+          fontSize: 13,
+          marginBottom: 10,
+        }}
+      >
+        Featured Project
+      </Text>
+      <Text
+        style={{
+          fontFamily: "Calibre-Medium",
+          fontSize: 28,
+          color: myMilkYellow,
+        }}
+      >
+        {title}
+      </Text>
+
+      <div style={{ position: "relative", top: 10, zIndex: 2 }}>
+        <div
+          style={{
+            backgroundColor: myLightNavy,
+            width: "120%",
+            height: 150,
+            left: textRightAlign ? "-20%" : "0%",
+            position: "absolute",
+            borderRadius: "2%",
+          }}
+        >
+          <Text
+            style={{
+              padding: 20,
+              color: myTextGrey,
+              fontSize: 18,
+            }}
+          >
+            {description}
+          </Text>
+        </div>
+      </div>
+      <div style={{ position: "relative", top: 150 }}>
+        <ul
+          style={{
+            fontFamily: "SFMono-Regular",
+            fontSize: 13,
+            color: myTextGrey,
+            listStyle: "none",
+            marginTop: 25,
+            marginBottom: 20,
+            padding: 0,
+          }}
+        >
+          {tools.map((tool) => {
+            return (
+              <li
+                style={{
+                  display: "inline",
+                  marginLeft: textRightAlign ? 10 : 0,
+                  marginRight: textRightAlign ? 0 : 10,
+                }}
+              >
+                {tool}
+              </li>
+            );
+          })}
+        </ul>
+        {githubLink || externalLink ? (
+          <div style={{ marginBottom: 10 }}>
+            {githubLink ? (
+              <a
+                href={githubLink}
+                style={{
+                  color:
+                    linkMouseFocused === "github" ? myMintGreen : myMilkYellow,
+                  marginLeft: textRightAlign ? 15 : 0,
+                  marginRight: textRightAlign ? 0 : 15,
+                }}
+                onMouseEnter={() => {
+                  setLinkMouseFocused("github");
+                }}
+                onMouseLeave={() => {
+                  setLinkMouseFocused("none");
+                }}
+              >
+                <GitHubIcon />
+              </a>
+            ) : null}
+            {externalLink ? (
+              <a
+                href={externalLink}
+                style={{
+                  color:
+                    linkMouseFocused === "external"
+                      ? myMintGreen
+                      : myMilkYellow,
+                  marginLeft: textRightAlign ? 15 : 0,
+                  marginRight: textRightAlign ? 0 : 15,
+                }}
+                onMouseEnter={() => {
+                  setLinkMouseFocused("external");
+                }}
+                onMouseLeave={() => {
+                  setLinkMouseFocused("none");
+                }}
+              >
+                <OpenInNewIcon />
+              </a>
+            ) : null}
+          </div>
+        ) : null}
+        {buttonLink ? (
+          <Button content={"Learn More"} link={buttonLink} />
+        ) : null}
+      </div>
+    </>
+  );
+}
+
 export interface FeaturedProjectWidgetProps {
   index: number;
   picture?: any;
@@ -33,137 +202,46 @@ export default function FeaturedProjectWidget({
   githubLink,
   buttonLink,
 }: FeaturedProjectWidgetProps) {
-  const [linkMouseFocused, setLinkMouseFocused] = useState("none");
-
-  const isEven = index % 2;
+  const isOdd = index % 2 === 1;
+  console.log(index, isOdd);
 
   return (
     <Grid container style={{ marginBottom: 100 }}>
-      <Grid item xs={7} style={{ position: "relative" }}>
-        <div
-          style={{
-            width: "100%",
-            backgroundColor: "#35425980", // myNavy with 80 Aplha
-            position: "absolute",
-            zIndex: 1,
-            height: "100%",
-          }}
-        />
-        <img
-          src={picture}
-          style={{ width: "100%" }}
-          alt={title + " profile Picture"}
-        />
-      </Grid>
-      <Grid item xs={5} style={{ textAlign: "right", marginTop: 10 }}>
-        <Text
-          style={{
-            color: myMintGreen,
-            fontFamily: "SFMono-Regular",
-            fontSize: 13,
-            marginBottom: 10,
-          }}
-        >
-          Featured Project
-        </Text>
-        <Text
-          style={{
-            fontFamily: "Calibre-Medium",
-            fontSize: 28,
-            color: myMilkYellow,
-          }}
-        >
-          {title}
-        </Text>
-
-        <div style={{ position: "relative", top: 10, zIndex: 2 }}>
-          <div
-            style={{
-              backgroundColor: myLightNavy,
-              width: "120%",
-              height: 150,
-              left: "-20%",
-              position: "absolute",
-              borderRadius: "2%",
-            }}
-          >
-            <Text
-              style={{
-                padding: 20,
-                color: myTextGrey,
-                fontSize: 18,
-              }}
-            >
-              {description}
-            </Text>
-          </div>
-        </div>
-        <div style={{ position: "relative", top: 150 }}>
-          <ul
-            style={{
-              fontFamily: "SFMono-Regular",
-              fontSize: 13,
-              color: myTextGrey,
-              listStyle: "none",
-              marginTop: 25,
-              marginBottom: 20,
-            }}
-          >
-            {tools.map((tool) => {
-              return (
-                <li style={{ display: "inline", marginLeft: 10 }}>{tool}</li>
-              );
-            })}
-          </ul>
-          {githubLink || externalLink ? (
-            <div style={{ marginBottom: 10 }}>
-              {githubLink ? (
-                <a
-                  href={githubLink}
-                  style={{
-                    color:
-                      linkMouseFocused === "github"
-                        ? myMintGreen
-                        : myMilkYellow,
-                    marginLeft: 15,
-                  }}
-                  onMouseEnter={() => {
-                    setLinkMouseFocused("github");
-                  }}
-                  onMouseLeave={() => {
-                    setLinkMouseFocused("none");
-                  }}
-                >
-                  <GitHubIcon />
-                </a>
-              ) : null}
-              {externalLink ? (
-                <a
-                  href={externalLink}
-                  style={{
-                    color:
-                      linkMouseFocused === "external"
-                        ? myMintGreen
-                        : myMilkYellow,
-                    marginLeft: 15,
-                  }}
-                  onMouseEnter={() => {
-                    setLinkMouseFocused("external");
-                  }}
-                  onMouseLeave={() => {
-                    setLinkMouseFocused("none");
-                  }}
-                >
-                  <OpenInNewIcon />
-                </a>
-              ) : null}
-            </div>
-          ) : null}
-          {buttonLink ? (
-            <Button content={"Learn More"} link={buttonLink} />
-          ) : null}
-        </div>
-      </Grid>
+      {!isOdd ? (
+        <>
+          <Grid item xs={7} style={{ position: "relative" }}>
+            <FeaturedProjectImage picture={picture} title={title} />
+          </Grid>
+          <Grid item xs={5} style={{ textAlign: "right", marginTop: 10 }}>
+            <FeaturedProjectDetails
+              textRightAlign={!isOdd}
+              title={title}
+              description={description}
+              tools={tools}
+              externalLink={externalLink}
+              githubLink={githubLink}
+              buttonLink={buttonLink}
+            />
+          </Grid>
+        </>
+      ) : (
+        <>
+          <Grid item xs={5} style={{ textAlign: "left", marginTop: 10 }}>
+            <FeaturedProjectDetails
+              textRightAlign={!isOdd}
+              title={title}
+              description={description}
+              tools={tools}
+              externalLink={externalLink}
+              githubLink={githubLink}
+              buttonLink={buttonLink}
+            />
+          </Grid>
+          <Grid item xs={7} style={{ position: "relative" }}>
+            <FeaturedProjectImage picture={picture} title={title} />
+          </Grid>
+        </>
+      )}
     </Grid>
   );
 }
