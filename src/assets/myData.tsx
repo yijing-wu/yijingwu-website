b355d362113eb50963a221ee41773a96759db6cb
yijing-wu <yijingwu.w@gmail.com>
2025-04-29 14:46:53 -0400

    Update about me

--- src/assets/myData.tsx
+++ src/assets/myData.tsx
@@ -10,7 +10,7 @@ const myBriefIntro =
 // About Page
 const myAboutMe = [
   "Hello! My name is Yijing. Welcome to my page!",
-  "I'm a software developer as well as a master's student at Northeastern University. I have experience with constructing web and mobile applications, cloud DevOps as well as Generative AI. ",
+  "I'm a software developer and have experience with constructing web and mobile applications, cloud DevOps as well as Generative AI. ",
   "I enjoy the process of developing, akin to solving puzzles, requiring a comprehensive understanding of structure, beginning with small components, and gradually assembling them into a cohesive whole.",
   "Previously, I honed my skills at Upduo, where I served as a full-stack engineer, crafting engaging and effective peer learning experiences for users. ",
   "Today, I'm diving into Cloud DevOps at Autodesk. My role involves building AWS cloud CI/CD pipelines and unleashing the potential of Gen AI.",
